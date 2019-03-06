f82ff7f6c8c215f03eab4d9313419670bec4b7ab
tunahanertekin <tertekin@etu.edu.tr>
2019-03-06 20:30:33 +0300

    BİL 481 - HW2

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -14,4 +14,38 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void testFirstBranch() {
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(0);
+    c = new Compute(mq);
+    assertEquals("Error where size=0 !", -1, c.countNumberOfOccurrences("string"));
+  }
+
+  @Test
+  public void testSecondBranch(){
+    MessageQueue mq=mock(MessageQueue.class);
+    //int randomSize=int(Math.random()*100)+6;//when size>6 and <100
+    //I put this line in comment because it did not give me a healthy test result.
+    when(mq.size()).thenReturn(6);
+    when(mq.contains("string")).thenReturn(false);
+    c=new Compute(mq);
+    assertEquals("Error where string does not contain string e ! ",0, c.countNumberOfOccurrences("string"));
+  }
+
+  @Test
+  public void testCountOccurrences(){
+    MessageQueue mq=mock(MessageQueue.class);
+    when(mq.size()).thenReturn(6);
+    when(mq.contains("t")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("s");
+    when(mq.getAt(1)).thenReturn("t");
+    when(mq.getAt(2)).thenReturn("r");
+    when(mq.getAt(3)).thenReturn("i");
+    when(mq.getAt(4)).thenReturn("n");
+    when(mq.getAt(5)).thenReturn("g");
+    c=new Compute(mq);
+    assertEquals("Error where counting 's' in 'string' !",1, c.countNumberOfOccurrences("t"));
+  }
 }
--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -13,4 +13,33 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testOneArg(){
+    assertFalse("Method with one argument must return false.", c.compute(10));
+  }
+
+  @Test
+  public void testEvenArgs(){
+    c=new Util();
+    assertFalse("Method with even # of arguments must return false", c.compute(4,5));
+  }
+
+  @Test
+  public void testDividedByArgument(){
+    c=new Util();
+    assertTrue("Sum of arguments is perfectly divided by at least one argument.",c.compute(3,4,5));
+  }
+  
+  @Test
+  public void testNotDividedByArgument(){
+    c=new Util();
+    assertFalse("Sum of arguments is perfectly divided by at least one argument.", c.compute(2,4,7));
+  }
+
+  @Test(expected=RuntimeException.class)
+  public void testArgEqualsZero(){
+    c=new Util();
+    assertFalse("An argument is equal to zero, runtime exception must be throwed.", c.compute(3,5,0));
+  }
 }
